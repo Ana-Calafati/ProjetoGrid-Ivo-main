646dfe4f3365c5e0486ee5b16718c19b45bd4675
Ana Grifoni <ana.grifoni@aluno.senai.br>
2026-04-09 17:30:43 -0300

    fazendo a imagem aparecer no banco de dados

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from model.comidas import mostrar_comidas
 
 app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return render_template("index.html")
+    comidas = mostrar_comidas()
+    return render_template("index.html", exibir_comidas=comidas)
 
 @app.route("/produto")
 def segunda_pagina():
